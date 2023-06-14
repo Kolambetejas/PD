560200cebdf580fc171db258b19ebb25566d961a
Tejas Kolambe <Kolambetejas79@gmail.c`om>
2023-06-14 15:11:44 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -8,23 +8,44 @@ function App(){
     );
 }
 function AppHeader() {
-    return (
-        <h1 className="bg-primary text-light p-3"> Header</h1>
+
+        return (
+            <div className="row bg-primary p-3 text-light fs-2 mb-0" >
+              <div className="col">Home</div>
+              <div className="col">Project</div>
+              <div className="col" >AboutUs</div>
+            </div>
+     
     );
  }
 function AppBody(){
     return(
         <>
-        <h1 className="bg-dark text-light p-3">AppBody</h1>
+        <div  className="bg-info alert alert-sucess fs3 mb-0">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis
+        rem corrupti ipsam, necessitatibus, ut tempore nisi aspernatur cumque
+        blanditiis quae quia. Corrupti, in ducimus. Ullam suscipit inventore
+        quam ex non. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Voluptatibus tempora eum cupiditate. Nobis id, facere iure debitis eum a
+        at, nulla placeat, tenetur tempore ratione adipisci deleniti alias
+        aspernatur voluptatem! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Perspiciatis rem corrupti ipsam, necessitatibus, ut
+        tempore nisi aspernatur cumque blanditiis quae quia. Corrupti, in
+        ducimus. Ullam suscipit inventore quam ex non. Lorem ipsum dolor sit
+        amet consectetur adipisicing elit. Voluptatibus tempora eum cupiditate.
+        Nobis id, facere iure debitis eum a at, nulla placeat, tenetur tempore
+        ratione adipisci deleniti alias aspernatur voluptatem!
+        </div>
         </>
     );
 }
 
 function AppFooter(){
     return(
-        <>
-        <h1 className="bg-secondary p-3 text-light">AppBody</h1>
-        </>
+     <div className="bg-secondary text-light d-flex justify-content-center align-item-center  " style={{ height :"100px"}}>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit.
+
+     </div>
     );
 }
 export default App;
