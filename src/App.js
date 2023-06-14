9bce002c66bfae6deac7356f7c4ee7131cc8621f
Tejas Kolambe <Kolambetejas79@gmail.c`om>
2023-06-14 11:55:58 +0530

    colour

--- src/App.js
+++ src/App.js
@@ -2,12 +2,29 @@ function App(){
     return(
         <>
         <AppHeader/>
+        <AppBody/>
+        <AppFooter/>
         </>
     );
 }
 function AppHeader() {
     return (
-        <h1> Header</h1>
+        <h1 className="bg-primary text-light p-3"> Header</h1>
+    );
+ }
+function AppBody(){
+    return(
+        <>
+        <h1 className="bg-dark text-light p-3">AppBody</h1>
+        </>
+    );
+}
+
+function AppFooter(){
+    return(
+        <>
+        <h1 className="bg-secondary p-3 text-light">AppBody</h1>
+        </>
     );
 }
 export default App;
