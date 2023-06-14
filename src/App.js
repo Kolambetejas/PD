6b9d9e0cb109abefae73af1610ffec5a4501565e
Tejas Kolambe <Kolambetejas79@gmail.c`om>
2023-06-14 11:21:45 +0530

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 function App(){
-  return(
-    <>
-    <h1>Hello world </h1>
-    </>
-  )
+    return(
+        <>
+        <AppHeader/>
+        </>
+    );
+}
+function AppHeader() {
+    return (
+        <h1> Header</h1>
+    );
 }
 export default App;
